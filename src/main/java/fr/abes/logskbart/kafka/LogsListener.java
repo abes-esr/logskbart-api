8a4e4ed100f0813e631f58ecd3fcbaccc02a8223
SamuelQuetin <quetin@abes.fr>
2023-12-11 10:01:49 +0100

    refactor multi listener kafka

--- src/main/java/fr/abes/logskbart/kafka/LogsListener.java
+++ src/main/java/fr/abes/logskbart/kafka/LogsListener.java
@@ -48,78 +48,80 @@ public class LogsListener {
 
     /**
      * Ecoute les topic de log d'erreurs et de fin de traitement bestPpn et génère un fichier err pour chaque fichier kbart
+     *
      * @param message le message kafka
      * @throws IOException exception levée
      */
-    @KafkaListener(topics = {"${topic.name.source.error}", "${topic.name.source.info}", "${topic.name.source.endoftraitement}"}, groupId = "logskbart", containerFactory = "kafkaLogsListenerContainerFactory")
+    @KafkaListener(topics = {"${topic.name.source.error}", "${topic.name.source.info}"}, groupId = "logskbart", containerFactory = "kafkaLogsListenerContainerFactory")
     public void listenInfoKbart2KafkaAndErrorKbart2Kafka(ConsumerRecord<String, String> message) throws IOException {
+        log.info("Error ou Info");
+        Kbart2KafkaDto dto = mapper.readValue(message.value(), Kbart2KafkaDto.class);
+        LogKbart entity = logsMapper.map(dto, LogKbart.class);
+        Timestamp timestamp = new Timestamp(message.timestamp());
+        entity.setTimestamp(new Date(timestamp.getTime()));
+        entity.setPackageName(message.key().split("\\[")[0]);
 
-        if (message.topic().equals(topicErrorKbart) || message.topic().equals(topicInfoKbart)) {
-            Kbart2KafkaDto dto = mapper.readValue(message.value(), Kbart2KafkaDto.class);
-            LogKbart entity = logsMapper.map(dto, LogKbart.class);
-            Timestamp timestamp = new Timestamp(message.timestamp());
-            entity.setTimestamp(new Date(timestamp.getTime()));
-            entity.setPackageName(message.key().replaceAll("\\[line\\s:\\s\\d+\\]", ""));
+        //  Si la ligne de log sur le topic errorkbart2kafka est de type ERROR
+        if (entity.getLevel().toString().equals("ERROR")) {
+            String nbrLine = message.key().split("\\[line:")[1].replace("]", "");
+            String fileName = entity.getPackageName().replace(".tsv", ".bad");
+            String line = nbrLine + "\t" + dto.getMessage();
 
-            //  Si la ligne de log sur le topic errorkbart2kafka est de type ERROR
-            if (entity.getLevel().toString().equals("ERROR")) {
-                String nbrLine = message.key().substring(message.key().indexOf(".tsv")+4).replaceAll("\\[line\\s:\\s", "").replaceAll("]", "");
-                String fileName = message.key().replaceAll(".tsv\\[line\\s:\\s\\d+\\]", ".bad");
-                String line = nbrLine + "\t" + dto.getMessage();
-
-                //  Vérifie qu'un fichier portant le nom du kbart en cours existe
-                Path of = Path.of(fileName);
-                if (Files.exists(of)) {
-                    //  Inscrit la ligne dedans
+            //  Vérifie qu'un fichier portant le nom du kbart en cours existe
+            Path of = Path.of(fileName);
+            if (Files.exists(of)) {
+                //  Inscrit la ligne dedans
+                Files.write(of, (line + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
+            } else if (!Files.exists(of)) {
+                try {
+                    //  Créer le fichier et inscrit la ligne dedans
+                    Files.createFile(of);
+                    //  Créer la ligne d'en-tête
+                    Files.write(of, ("LINE\tMESSAGE" + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
+                    //  Inscrit les informations sur la ligne
                     Files.write(of, (line + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
-                } else if (!Files.exists(of)) {
-                    try {
-                        //  Créer le fichier et inscrit la ligne dedans
-                        Files.createFile(of);
-                        //  Créer la ligne d'en-tête
-                        Files.write(of, ("LINE\tMESSAGE" + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
-                        //  Inscrit les informations sur la ligne
-                        Files.write(of, (line + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
-                        log.info("Fichier temporaire créé.");
-                    } catch (SecurityException | IOException e) {
-                        log.error("Erreur lors de la création du fichier temporaire. " + e);
-                        throw new RuntimeException(e);
-                    }
+                    log.info("Fichier temporaire créé.");
+                } catch (SecurityException | IOException e) {
+                    log.error("Erreur lors de la création du fichier temporaire. " + e);
+                    throw new RuntimeException(e);
                 }
             }
+        }
 
-            //  Inscrit l'entity en BDD
-            repository.save(entity);
+        //  Inscrit l'entity en BDD
+        repository.save(entity);
+    }
 
-        } else if (message.topic().equals(topicEndOfTraitement)) {    //  Si la ligne sur le topic bestppn.endoftraitment contient OK
-
-            //  Créer un nouveau Path avec le FileName (en remplaçant l'extension par .err)
-            Path source = null;
-            for (Header header : message.headers().toArray()) {
-                if (header.key().equals("FileName")) {
-                    source = Path.of(new String(header.value()).replaceAll(".tsv", ".bad"));
-                    break;
-                }
+    @KafkaListener(topics = {"bestppn.endoftraitment"}, groupId = "logskbart", containerFactory = "kafkaLogsListenerContainerFactory")
+    public void listener(ConsumerRecord<String, String> message) throws IOException {
+        log.info("End Of Traitement");
+        //  Créer un nouveau Path avec le FileName (en remplaçant l'extension par .err)
+        Path source = null;
+        for (Header header : message.headers().toArray()) {
+            if (header.key().equals("FileName")) {
+                source = Path.of(new String(header.value()).replace(".tsv", ".bad"));
+                break;
             }
+        }
 
-            //  Copie le fichier existant vers le répertoire temporaire en ajoutant sa date de création
-            if (source != null && Files.exists(source)) {
-                LocalDateTime time = LocalDateTime.now();
-                DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss", Locale.FRANCE);
-                String date = format.format(time);
+        //  Copie le fichier existant vers le répertoire temporaire en ajoutant sa date de création
+        if (source != null && Files.exists(source)) {
 
-                //  Vérification du chemin et création si inexistant
-                String tempLog = "tempLog/";
-                File chemin = new File("tempLog/");
-                if (!chemin.isDirectory()) {
-                    Files.createDirectory(Paths.get(tempLog));
-                }
-                Path target = Path.of("tempLog\\" + date + "_" + source);
+            LocalDateTime time = LocalDateTime.now();
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss", Locale.FRANCE);
+            String date = format.format(time);
 
-                //  Déplacement du fichier
-                Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
-                log.info("Fichier de log transféré dans le dossier temporaire.");
+            //  Vérification du chemin et création si inexistant
+            String tempLog = "tempLog/";
+            File chemin = new File(tempLog);
+            if (!chemin.isDirectory()) {
+                Files.createDirectory(Paths.get(tempLog));
             }
+            Path target = Path.of("tempLog\\" + date + "_" + source);
+
+            //  Déplacement du fichier
+            Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
+            log.info("Fichier de log transféré dans le dossier temporaire.");
         }
     }
 }
