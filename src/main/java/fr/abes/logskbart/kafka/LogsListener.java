a7f037ab0969125cfbbec2f9893c3120c295d672
EryneKL <97091460+EryneKL@users.noreply.github.com>
2024-04-05 14:45:21 +0200

    FEAT : CDE-403-sur-un-gros-fichier-le-bad-ecrase-son-contenu-au-fil-du-traitement      - création d'un listener spécifique et envoi du fichier .bad uniquement à la fin du traitement

--- src/main/java/fr/abes/logskbart/kafka/LogsListener.java
+++ src/main/java/fr/abes/logskbart/kafka/LogsListener.java
@@ -8,6 +8,7 @@ import fr.abes.logskbart.service.EmailService;
 import fr.abes.logskbart.utils.UtilsMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.header.Header;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +16,13 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileLock;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
+import java.nio.file.*;
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -54,7 +56,7 @@ public class LogsListener {
      * @throws IOException exception levée
      */
     @KafkaListener(topics = {"${topic.name.source.error}"}, groupId = "${topic.groupid.source}", containerFactory = "kafkaLogsListenerContainerFactory")
-    public void listenInfoKbart2KafkaAndErrorKbart2Kafka(ConsumerRecord<String, String> message) throws IOException, InterruptedException {
+    public void listenInfoKbart2KafkaAndErrorKbart2Kafka(ConsumerRecord<String, String> message) throws IOException {
         LogKbartDto dto = mapper.readValue(message.value(), LogKbartDto.class);
         LogKbart logKbart = logsMapper.map(dto, LogKbart.class);
 
@@ -71,11 +73,12 @@ public class LogsListener {
 
         // Vérifie qu'un fichier portant le nom du kbart en cours existe
         if (!logKbart.getPackageName().contains("ctx:package") && !logKbart.getPackageName().contains("_FORCE")) {
-            Path tempPath = Path.of("tempLog");
+
+            Path tempPath = Path.of("tempLogLocal");
             if(!Files.exists(tempPath)) {
                 Files.createDirectory(tempPath);
             }
-            Path of = Path.of("tempLog" + File.separator + logKbart.getPackageName().replace(".tsv", ".bad"));
+            Path of = Path.of("tempLogLocal" + File.separator + logKbart.getPackageName().replace(".tsv", ".bad"));
 
             //  Si la ligne de log sur le topic est de type ERROR
             if (logKbart.getLevel().toString().equals("ERROR")) {
@@ -94,45 +97,52 @@ public class LogsListener {
                 String line = nbLineOrigine + "\t" + logKbart.getMessage();
 
                 if (Files.exists(of)) {
-                    FileOutputStream fos = new FileOutputStream(String.valueOf(of), true);
-                    FileLock lock = fos.getChannel().lock();
-                    try (fos) {
-                        //  Inscrit la ligne dedans
-                        Files.write(of, (line + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
-                    } finally {
-                        lock.release();
-                        lock.close();
-                    }
+                    //  Inscrit la ligne dedans
+                    Files.write(of, (line + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
                 } else if (!Files.exists(of)) {
                     try {
-                        FileOutputStream fos = new FileOutputStream(String.valueOf(of), true);
-                        FileLock lock = fos.getChannel().lock();
-                        try (fos) {
-                            //  Créer le fichier et inscrit la ligne dedans
-                            Files.createFile(of);
-                            //  Créer la ligne d'en-tête
-                            Files.write(of, ("LINE\tMESSAGE" + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
-                            //  Inscrit les informations sur la ligne
-                            Files.write(of, (line + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
-                            log.info("Fichier temporaire créé.");
-                        } finally {
-                            lock.release();
-                            lock.close();
-                        }
-                    } catch (SecurityException | IOException e) {
+                        //  Créer le fichier et inscrit la ligne dedans
+                        Files.createFile(of);
+                        //  Créer la ligne d'en-tête
+                        Files.write(of, ("LINE\tMESSAGE" + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
+                        //  Inscrit les informations sur la ligne
+                        Files.write(of, (line + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
+                        log.info("Fichier temporaire créé.");
+                      } catch (SecurityException | IOException e) {
                         log.error("Erreur lors de la création du fichier temporaire. " + e);
                         throw new RuntimeException(e);
                     }
-                }
-            } else if (logKbart.getLevel().toString().equals("INFO") && logKbart.getMessage().contains("Traitement terminé pour fichier " + logKbart.getPackageName())) {
-                // Envoi du mail uniquement si le fichier temporaire a été créé
-                if (Files.exists(of)) {
-                    Thread.sleep(20000); // pour attendre que tous les threads de best-ppn-api aient terminé leurs traitements
-                    emailService.sendMailWithAttachment(logKbart.getPackageName(), of);
                 }
             }
         }
         //  Inscrit l'entity en BDD
         repository.save(logKbart);
+    }
+
+    /**
+     * Ecoute les topic de log d'erreurs et de fin de traitement bestPpn et génère un fichier err pour chaque fichier kbart
+     *
+     * @param message le message kafka
+     * @throws IOException exception levée
+     */
+    @KafkaListener(topics = {"${topic.name.source.error}"}, groupId = "${topic.groupid.source}", containerFactory = "kafkaLogsListenerContainerFactory")
+    public void listenBestppnapiEndoftraitmentMessage(ConsumerRecord<String, String> message) throws IOException, InterruptedException {
+        LogKbartDto dto = mapper.readValue(message.value(), LogKbartDto.class);
+        LogKbart logKbart = logsMapper.map(dto, LogKbart.class);
+        if (logKbart.getLevel().toString().equals("INFO") && logKbart.getMessage().contains("Traitement terminé pour fichier " + logKbart.getPackageName())) {
+            // Envoi du mail uniquement si le fichier temporaire a été créé
+            Path of = Path.of("tempLogLocal" + File.separator + logKbart.getPackageName().replace(".tsv", ".bad"));
+            if (Files.exists(of)) {
+                Thread.sleep(20000); // pour attendre que tous les threads de best-ppn-api aient terminé leurs traitements
+                //  Copie le fichier existant vers le répertoire temporaire en ajoutant sa date de création
+                if (of != null && Files.exists(of)) {
+                    Path target = Path.of("tempLog" + File.separator + logKbart.getPackageName().replace(".tsv", ".bad"));
+                    //  Déplacement du fichier
+                    Files.move(of, target, StandardCopyOption.REPLACE_EXISTING);
+                    log.info("Fichier de log transféré dans le dossier temporaire.");
+                }
+                emailService.sendMailWithAttachment(logKbart.getPackageName(), of);
+            }
+        }
     }
 }
