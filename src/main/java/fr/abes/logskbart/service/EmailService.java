dd4fccfde5cb155905a53a8ade06773cf98bc2ee
SamuelQuetin <quetin@abes.fr>
2025-01-24 14:22:20 +0100

    tentative Fichier clickable

--- src/main/java/fr/abes/logskbart/service/EmailService.java
+++ src/main/java/fr/abes/logskbart/service/EmailService.java
@@ -35,7 +35,7 @@ public class EmailService {
 
     public void sendEmail(String packageName) {
         //  Création du mail
-        String requestJson = mailToJSON(this.recipient, "[KBART2BACON : erreurs]" + getTag() + " " + packageName, "Cliquez pour télécharger le fichier .bad : " + serveurUrl + packageName.replace(".tsv", ".bad"));
+        String requestJson = mailToJSON(this.recipient, "[KBART2BACON : erreurs]" + getTag() + " " + packageName, "<a href=\"" + serveurUrl + packageName.replace(".tsv", ".bad") + "\">Cliquez pour télécharger le fichier .bad</a>");
 
         //  Envoi du message par mail
         sendMail(requestJson);
